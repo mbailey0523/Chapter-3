b656840738c4eb95216a4d81f167ad5c21858639
mbailey0523 <morgan.colbert@myemail.indwes.edu>
2015-09-23 15:56:47 -0400

    Update and rename Ex1_ExpressionCalculation.asm to Ex1_ExpressionCalculationMORGANBAILEY.asm

--- Ex1_ExpressionCalculationMORGANBAILEY.asm
+++ Ex1_ExpressionCalculationMORGANBAILEY.asm
@@ -11,10 +11,19 @@ EBX, ECX, and EDX registers.
 .model flat,stdcall
 .stack 4096
 ExitProcess proto,dwExitCode:dword
+
 .code
 main PROC
+	mov eax,5		
+	add eax,6	;add A and B
+				
+	mov ecx,7		
+	add ecx,8	;add C and D
+				
+				
+	sub ecx,eax	;subtract CD from AB
 
-	
+	mov ebx,ecx
 
 	INVOKE ExitProcess,0
 main ENDP
